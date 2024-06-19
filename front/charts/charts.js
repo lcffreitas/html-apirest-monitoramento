e8566a120210c85949ca60ce764530ba4fb57d66
Lucas <lcffreitas1@gmail.com>
2024-06-19 09:05:23 -0300

    charts update

--- front/charts/charts.js
+++ front/charts/charts.js
@@ -1,95 +1,16 @@
-let arr = [['Dispositivo', 'Total']];
+import { renderBarChart } from './barChart/bar.js';
+import { renderLineChart } from './lineChart/line.js';
+import { renderPieChart } from './pieChart/pie.js';
 
-function getData() {
-  fetch('http://127.0.0.1:5000/monitoramento/grafico1')
-    .then((response) => response.json())
-    .then((data) => {
-      data.slice(0, 5).forEach((item) => {
-        arr.push([item.dispositivo, item.TotalRegistros]);
-      });
-      drawCharts();
-    })
-    .catch((error) => console.error('Erro ao obter dados:', error));
-}
-
-function drawCharts() {
+export function drawCharts() {
   loadGoogleCharts(() => {
-    renderPieChart();
-    renderBarChart();
-    renderLineChart();
+    if (document.getElementById('pie_chart')) renderPieChart();
+    if (document.getElementById('bar_chart')) renderBarChart();
+    if (document.getElementById('line_chart')) renderLineChart();
   });
 }
 
 function loadGoogleCharts(callback) {
-  google.charts.load('current', { packages: ['corechart', 'bar'] });
+  google.charts.load('current', { packages: ['corechart', 'bar', 'gauge'] });
   google.charts.setOnLoadCallback(callback);
 }
-
-function renderPieChart() {
-  const data = google.visualization.arrayToDataTable(arr);
-  const options = {
-    title: 'Total Dipositivos - Pizza',
-    colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-    fontSize: 14,
-    legend: { position: 'bottom', textStyle: { color: 'blue', fontSize: 16 } },
-  };
-  const chart = new google.visualization.PieChart(
-    document.getElementById('pie_chart')
-  );
-  chart.draw(data, options);
-}
-
-function renderBarChart() {
-  const data = google.visualization.arrayToDataTable(arr);
-  const options = {
-    chart: {
-      title: 'Total Dipositivos - Barras',
-    },
-    bars: 'horizontal',
-    colors: ['#1b9e77', '#d95f02', '#7570b3'],
-    hAxis: {
-      title: 'Total',
-      minValue: 0,
-      textStyle: { color: '#333' },
-      titleTextStyle: { color: '#1b9e77' },
-    },
-    vAxis: {
-      title: 'Dispositivo',
-      textStyle: { color: '#333' },
-      titleTextStyle: { color: '#1b9e77' },
-    },
-    fontName: 'Arial',
-    fontSize: 14,
-  };
-  const chart = new google.charts.Bar(
-    document.getElementById('barchart_material')
-  );
-  chart.draw(data, options);
-}
-
-function renderLineChart() {
-  const data = google.visualization.arrayToDataTable(arr);
-  const options = {
-    title: 'Total Dipositivos - Linha',
-    colors: ['#a52714', '#097138'],
-    hAxis: {
-      title: 'Dispositivo',
-      titleTextStyle: { color: '#333' },
-    },
-    vAxis: {
-      title: 'Total',
-      minValue: 0,
-      titleTextStyle: { color: '#333' },
-    },
-    fontName: 'Arial',
-    fontSize: 14,
-    areaOpacity: 0.2,
-    legend: { position: 'top', textStyle: { color: '#333', fontSize: 16 } },
-  };
-  const chart = new google.visualization.LineChart(
-    document.getElementById('chart_div')
-  );
-  chart.draw(data, options);
-}
-
-getData();
